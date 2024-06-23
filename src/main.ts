3669a1f3c794e2f10c9fa2a8c00c28513240e2e9
Belfor Acuna <b.acuna02@ufromail.cl>
2024-06-23 15:14:33 -0400

    feat: :rocket: Enable cors from *
    Implementation of Cors allowing traffic from * and allowing credetentials header

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
